7d898e224b0e21ed3a697bc386f60f7846a610fc
Bah <abdourahmanebah273@yahoo.fr>
2018-03-09 21:20:13 +0100

    Q1

--- interaction.js
+++ interaction.js
@@ -3,6 +3,12 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
+    this.canvas = canvas;
+    this.interactor = interactor;
+    this.Xstr = 0;
+    this.Ystr = 0;
+    this.Xend = 0;
+    this.Yend = 0;
 
 	// Developper les 3 fonctions gérant les événements
 
